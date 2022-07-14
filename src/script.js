cd076278863088216cecb5ecaa35ec891e7b76df
Mateus Abelli <43862225+mateusabelli@users.noreply.github.com>
2022-07-14 19:23:47 -0300

    refactor: project structure and code cleanup
    - feat: add assets from previous linked script
    - refactor: major cleanup of unnecessary accessibility tags
    - refactor: css and class names
    - refactor: folder structure to accommodate more complexity

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
-const openModal = document.querySelector(".transaction-button");
+const openModal = document.querySelector(".registry-button");
 const closeModal = document.querySelector(".close-button");
-const modal = document.querySelector(".transaction-modal");
+const modal = document.querySelector(".registry-modal");
 
 openModal.addEventListener("click", () => modal.showModal());
 closeModal.addEventListener("click", () => modal.close());
